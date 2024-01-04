a3c6df97f44e8d0544ba8a1b0ad3b32080abed80
muxammad <ibrohimovmuhammad2020@gmail.com>
2024-01-04 17:41:59 +0100

    extracting the data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import selectorlib
 
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
-
+URL1 = "https://service.berlin.de/terminvereinbarung/termin/taken/#&termin=1&dienstleister=327437&anliegen[]=120691"
 
 
 def scrape(URL):
@@ -11,6 +11,13 @@ def scrape(URL):
     source = responce.text
     return source
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
 
 if __name__ == "__main__":
-    print(scrape(URL))
+    source = scrape(URL)
+    result = extract(source)
+    print(result)
